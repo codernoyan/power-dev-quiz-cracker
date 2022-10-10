d29cbba1730fe16d8c6eef9ccbe817dd5f4c8189
codernoyan <coder.noyan@gmail.com>
2022-10-10 23:41:06 +0600

    router deploy redirect file added

--- src/components/Quizzes/Quizzes.js
+++ src/components/Quizzes/Quizzes.js
@@ -6,10 +6,10 @@ const Quizzes = () => {
     const quizData = useContext(QuizContext);
     // console.log(quizData)
     return (
-        <section className="py-6 sm:py-12">
+        <section className="py-6 sm:py-4">
             <div className="container p-6 mx-auto space-y-8">
                 <div className="space-y-2 text-center">
-                    <h2 className="text-3xl font-bold">Quize Topics</h2>
+                    <h2 className="text-3xl font-bold">Quiz Topics</h2>
                 </div>
                 <div className="grid grid-cols-1 gap-x-4 gap-y-8 md:grid-cols-2 lg:grid-cols-4">
                     {
