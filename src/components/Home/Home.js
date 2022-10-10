9c55de10da592ae7d6e65f2202b4ec224495890b
codernoyan <coder.noyan@gmail.com>
2022-10-10 22:53:28 +0600

    header added on home route

---
+++ src/components/Home/Home.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import headerImage from '../../assets/hero-image.jpg';
+
+const Home = () => {
+    return (
+        <>
+            <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-8">
+                <div className="p-8 rounded shadow-xl sm:p-6">
+                    <div className="flex flex-col items-center lg:flex-row">
+                        <div className="mb-6 lg:mb-0 lg:w-1/2 lg:pr-5">
+                           <img className='h-52' src={headerImage} alt="" />
+                        </div>
+                        <div className="lg:w-1/2">
+                            <p className="mb-4 text-base text-gray-700">
+                                Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                                accusantium doloremque rem aperiam, eaque ipsa quae. Sed ut
+                                perspiciatis unde omnis iste. Sed ut perspiciatis unde omnis iste
+                                error sit voluptatem accusantium doloremque rem aperiam.
+                            </p>
+                            <a
+                                href="/"
+                                aria-label=""
+                                className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
+                            >
+                                Learn more
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Home;
--- src/routes/router.js
+++ src/routes/router.js
@@ -1,9 +1,22 @@
 import { createBrowserRouter } from "react-router-dom";
+import ErrorPage from "../components/ErrorPage/ErrorPage";
+import Home from "../components/Home/Home";
 import Root from "../layouts/Root";
 
 export const router = createBrowserRouter([
     {
         path: '/',
-        element: <Root></Root>
+        element: <Root></Root>,
+        errorElement: <ErrorPage></ErrorPage>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            },
+            {
+                path: 'home',
+                element: <Home></Home>
+            }
+        ]
     }
 ])
