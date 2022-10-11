07b1d49798a3be427d5879ffed3e707bc09c48c6
codernoyan <coder.noyan@gmail.com>
2022-10-11 14:30:34 +0600

    added index on quiz questions

--- src/components/Questions/Questions.js
+++ src/components/Questions/Questions.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import Answer from '../Answer/Answer';
 
-const Questions = ({ question }) => {
+const Questions = ({ question, index }) => {
     console.log(question)
-    const { options, id, index, correctAnswer } = question;
+    const { options, id, correctAnswer } = question;
 
     return (
         <div>
             <div className='p-4 bg-slate-50 shadow-md mb-4'>
                 <div className='mb-4'>
-                    <h4 className='text-xl font-bold'>{question.question}</h4>
+                    {/* <h4 className='text-xl font-bold'>{`Quiz no.${index + 1} ${question.question}`}</h4> */}
+                    <h4 className='text-xl font-bold'>Quiz no.{index + 1} <span>{question.question}</span></h4>
                 </div>
                 <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
                     {
