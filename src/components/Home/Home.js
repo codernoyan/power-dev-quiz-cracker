90ea180923d2c41c96b43594148e89df2d1b92af
codernoyan <coder.noyan@gmail.com>
2022-10-11 22:25:29 +0600

    header recreated

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,23 +5,6 @@ import Quizzes from '../Quizzes/Quizzes';
 const Home = () => {
     return (
         <div>
-            {/* <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-8">
-                <div className="p-8 rounded shadow-xl sm:p-6">
-                    <div className="flex flex-col items-center lg:flex-row">
-                        <div className="mb-6 lg:mb-0 lg:w-1/2 lg:pr-5">
-                            <img className='h-52' src={headerImage} alt="" />
-                        </div>
-                        <div className="lg:w-1/2">
-                            <p className="mb-4 text-base text-gray-700">
-                                Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-                                accusantium doloremque rem aperiam, eaque ipsa quae. Sed ut
-                                perspiciatis unde omnis iste. Sed ut perspiciatis unde omnis iste
-                                error sit voluptatem accusantium doloremque rem aperiam.
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div> */}
             <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-8">
                 <div className="flex flex-col max-w-screen-lg overflow-hidden bg-white border rounded shadow-sm lg:flex-row sm:mx-auto">
                     <div className="relative lg:w-1/2">
@@ -43,10 +26,7 @@ const Home = () => {
                             Power Dev
                         </h5>
                         <p className="mb-5 text-gray-800">
-                            <span className="font-bold">Lorem ipsum</span> dolor sit amet,
-                            consectetur adipiscing elit. Etiam sem neque, molestie sit amet
-                            venenatis et, dignissim ut erat. Sed aliquet velit id dui eleifend,
-                            sed consequat odio sollicitudin.
+                            <span className="font-bold">Power Dev</span> is a platform that developers can intern here for a certain time. We build web apps via MERN stack. Developer can achieve their skills more effectively.
                         </p>
                     </div>
                 </div>
